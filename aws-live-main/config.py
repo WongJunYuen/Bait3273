373a7e28c2e0a282199f2085efe3efa0505686c0
WongJunYuen <108725028+WongJunYuen@users.noreply.github.com>
2023-04-13 15:25:22 +0800

    Add files via upload

---
+++ aws-live-main/config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cvxatcorqdgv.us-east-1.rds.amazonaws.com"
+customuser = "awsuser1"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "wongjunyuen-employee"
+customregion = "us-east-1"
